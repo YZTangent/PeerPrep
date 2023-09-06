ee71e2fc7a774ae16df93bc0800def947b0cffc2
cjyothika <jordynreina4@gmail.com>
2023-09-07 00:18:57 +0800

    Translate HTML to Angular code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+  questions: any[] = [];
+  currentIndex: Number = 0;
+  
+  ngOnInit(): void {
+  }
+
+  saveQuestions() {
+      localStorage.setItem("qs", JSON.stringify(this.questions));
+  }
+
+  view(i: number) {
+      this.currentIndex = i;
+  }
+
+  addItem(formData: any) {
+      this.questions?.push(formData.value)
+      this.saveQuestions();
+  }
+
+  editItem(index: number, formData: any) {
+    this.questions[index] = formData;
+  }
+
+  deleteItem(i: number) {
+    this.questions.splice(i);
+  }
+  
 }
