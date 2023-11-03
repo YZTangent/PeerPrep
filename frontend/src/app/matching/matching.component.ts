5ebfd9d7e15d75d041e26c94066d35def80a8881
cjyothika <jordynreina4@gmail.com>
2023-11-04 00:38:40 +0800

    Add timeout bar

--- frontend/src/app/matching/matching.component.ts
+++ frontend/src/app/matching/matching.component.ts
@@ -3,7 +3,7 @@ import { NgForm } from '@angular/forms'
 import { Router } from '@angular/router';
 import { MatchingService } from '../_services/matching.service'
 import { StorageService } from '../_services/storage.service'
-import { timeoutWith, throwError } from 'rxjs'
+import { timeoutWith, throwError, count, interval } from 'rxjs'
 
 @Component({
   selector: 'app-matching',
@@ -20,6 +20,8 @@ export class MatchingComponent {
 
   currUser = this.storageService.getUser().username
 
+  timer = {s: 0, mn: 0};
+
   constructor(
     private router: Router,
     private matchingService: MatchingService,
@@ -30,10 +32,23 @@ export class MatchingComponent {
   }
 
   getMatch(userDetails: any) {
+    var countDown = setInterval(() => {
+      this.timer.s += 1;
+      document.getElementById("bar")!.style.width = (this.timer.s/30)*100+"%";
+      document.getElementById("bar")!.innerHTML = this.timer.s + "s";
+      if (this.timer.s > 30) {
+        this.timer.s = 0;
+        this.timer.mn += 1;
+      }
+    }, 1000);
+
+    // setTimeout(() => clearInterval(countDown), 30000);
+
     this.matchingService.enqueue(userDetails).pipe(timeoutWith(30000, throwError(() => {
       this.matchingService.dequeue(userDetails["userid"]).subscribe((res) => {
         this.match = "Your request timed out!"
-        this.requested = false
+        setTimeout(() => this.requested = false, 5000);
+        clearInterval(countDown)
         this.getQueueLength()
       })
     }))).subscribe((res) => {
