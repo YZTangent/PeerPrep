6c9501e37ba996529118d71a6555fa96a47b8151
RX <R.@Richards-MacBook-Pro.local>
2023-11-06 16:38:40 +0800

    removing comment

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,11 +20,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.name).toEqual('frontend');
   });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
-  // });
 });
