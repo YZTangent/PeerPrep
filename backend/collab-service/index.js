0f5da813542e23019365d1d980b858e90ca04065
chia-yh <0.chiayuhong.0@gmail.com>
2023-11-15 22:58:04 +0800

    Fix

--- backend/collab-service/index.js
+++ backend/collab-service/index.js
@@ -31,51 +31,51 @@ io.on("connection", (socket) => {
     console.log(`${socket.id} recovered connection state.`);
   } else {
     console.log(`${socket.id} connected.`);
-
-    let matchedRoomId;
-
-    socket.on("join", (roomId) => {
-      console.log(`${socket.id} joining room ${roomId}`)
-      matchedRoomId = roomId
-      socket.join(matchedRoomId);
-    })
-
-    socket.on("leave", (m) => {
-      console.log(`${socket.id} leaving room ${matchedRoomId}`);
-      socket.leave(matchedRoomId);
-      socket.broadcast.to(matchedRoomId).emit("leave", `${socket.id} leaving room ${matchedRoomId}`);
-      console.log(`Existing rooms: ${Array.from(io.sockets.adapter.rooms.keys())}.`);
-    })
-
-    socket.on("question", (question) => {
-      console.log(`${socket.id} emitting question ${question.questionTitle} to ${matchedRoomId}.`);
-      io.to(matchedRoomId).emit("question", question);
-    })
-
-    socket.on("change", (change) => {
-      console.log(`${socket.id} emitting change ${change} to ${matchedRoomId}.`);
-      socket.broadcast.to(matchedRoomId).emit("change", change);
-    })
-
-    socket.on("message", (message) => {
-      console.log(`${socket.id} emitting message ${message} to ${matchedRoomId}`);
-      socket.broadcast.to(matchedRoomId).emit("message", message);
-    })
-
-    socket.on("disconnect", (reason) => {
-      console.log(`${socket.id} disconnected due to: ${reason}.`);
-      if (reason != "transport close") {
-        io.to(matchedRoomId).emit("message", `User disconnected.`);
-      }
-      console.log(`Existing rooms: ${Array.from(io.sockets.adapter.rooms.keys())}.`);
-    })
-
-    socket.on("request", (question) => {
-      console.log(`${socket.id} requested a change of question to ${question.questionId} ${matchedRoomId}`);
-      if (io.engine.clientsCount == 1) {
-        io.to(matchedRoomId).emit("question", question);
-      } 
-      socket.broadcast.to(matchedRoomId).emit("request", question);
-    }) 
   }
+
+  let matchedRoomId;
+
+  socket.on("join", (roomId) => {
+    console.log(`${socket.id} joining room ${roomId}`)
+    matchedRoomId = roomId
+    socket.join(matchedRoomId);
+  })
+
+  socket.on("leave", (m) => {
+    console.log(`${socket.id} leaving room ${matchedRoomId}`);
+    socket.leave(matchedRoomId);
+    socket.broadcast.to(matchedRoomId).emit("leave", `${socket.id} leaving room ${matchedRoomId}`);
+    console.log(`Existing rooms: ${Array.from(io.sockets.adapter.rooms.keys())}.`);
+  })
+
+  socket.on("question", (question) => {
+    console.log(`${socket.id} emitting question ${question.questionTitle} to ${matchedRoomId}.`);
+    io.to(matchedRoomId).emit("question", question);
+  })
+
+  socket.on("change", (change) => {
+    console.log(`${socket.id} emitting change ${change} to ${matchedRoomId}.`);
+    socket.broadcast.to(matchedRoomId).emit("change", change);
+  })
+
+  socket.on("message", (message) => {
+    console.log(`${socket.id} emitting message ${message} to ${matchedRoomId}`);
+    socket.broadcast.to(matchedRoomId).emit("message", message);
+  })
+
+  socket.on("disconnect", (reason) => {
+    console.log(`${socket.id} disconnected due to: ${reason}.`);
+    if (reason != "transport close") {
+      io.to(matchedRoomId).emit("message", `User disconnected.`);
+    }
+    console.log(`Existing rooms: ${Array.from(io.sockets.adapter.rooms.keys())}.`);
+  })
+
+  socket.on("request", (question) => {
+    console.log(`${socket.id} requested a change of question to ${question.questionId} ${matchedRoomId}`);
+    if (io.engine.clientsCount == 1) {
+      io.to(matchedRoomId).emit("question", question);
+    } 
+    socket.broadcast.to(matchedRoomId).emit("request", question);
+  })
 });
